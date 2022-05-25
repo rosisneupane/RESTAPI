9543e7d7fa98befba86126ee885c4c5581faa9fb
rosis <rosisneupane1@gmail.com>
2022-05-25 12:02:45 +0545

    Class Based view Complete

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import article_list,article_detail,ArticleAPIView
+from .views import article_list,article_detail,ArticleAPIView,ArticleDetails
 urlpatterns = [
     # path('articles/',article_list),
     path('articles/',ArticleAPIView.as_view()),
-    path('article/<int:pk>/',article_detail)
+    path('article/<int:id>/',ArticleDetails.as_view())
 ]
